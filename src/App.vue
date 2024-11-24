773151ced8fa6ced48351c4b711304e36d8a334e
David MsF <masifjizzwa@gmail.com>
2024-11-24 20:18:23 +0100

    to jsem zvědav

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {ref} from "vue";
+
+const nahodneCislo = ref(Math.floor(Math.random()*50));
+setInterval (() => {
+  nahodneCislo.value = (Math.floor(Math.random()*50));
+},2000 );
+
+
+</script>
+
+
+<template>
+  <h1>Ternární operátor </h1>
+  <h2>Úkol v rámci studia</h2>
+
+
+<p>Padlo číslo: {{ nahodneCislo  }}</p>
+<p>číslo je {{  (nahodneCislo %2===0) ? "sudé" : "liché" }}</p>
+<p>číslo je menší než 25: {{  (nahodneCislo < 25) ? "Ano" : "Ne"  }}</p>
+
+
+
+
+
+
+
+</template>
+
+
+
+<style scoped>
+
+
+
+</style>
