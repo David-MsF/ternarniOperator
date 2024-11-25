5a854939e873a52c461890a0981c34b02536979e
David MsF <masifjizzwa@gmail.com>
2024-11-25 22:03:20 +0100

    v-bind vue

--- src/components/DirektivaVBind.vue
+++ src/components/DirektivaVBind.vue
@@ -1,11 +1,11 @@
 <script setup>
-import {ref, computed} from "vue";
+import { ref, computed } from "vue";
 
-const hodnota = ref(0) ;
-function pridejHodnotu () {
-   if (hodnota.value < 10) {
-    hodnota.value++
-   }
+const hodnota = ref(0);
+function pridejHodnotu() {
+    if (hodnota.value < 10) {
+        hodnota.value++
+    }
 }
 
 function resetHodnotu() {
@@ -13,7 +13,7 @@ function resetHodnotu() {
 }
 
 
-const vypnoutTlacitko = computed(() => hodnota.value >=10 );
+const vypnoutTlacitko = computed(() => hodnota.value >= 10);
 
 </script>
 
@@ -23,12 +23,10 @@ const vypnoutTlacitko = computed(() => hodnota.value >=10 );
     <h2>po dosažení 10ti se tlačítko vypne</h2>
     <button v-on:click="pridejHodnotu()" :disabled="vypnoutTlacitko"> Přidej hodnotu o +1 </button>
     <button v-on:click="resetHodnotu"> Resetuj hodnotu </button>
-    <p> {{ hodnota  }}</p>
+    <p> {{ hodnota }}</p>
 
 </template>
 
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
