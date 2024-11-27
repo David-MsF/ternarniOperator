e04292149dd10890654634bb5a36a6bcd75991ff
David MsF <masifjizzwa@gmail.com>
2024-11-27 14:38:19 +0100

    fix poměr stran

--- src/components/ZvetseniZmenseni.vue
+++ src/components/ZvetseniZmenseni.vue
@@ -4,15 +4,16 @@ import { ref, computed } from "vue";
 const hodnotaButton = ref(5);
 const pridaniMaxu = ref('');
 
-    function zmenHodnotu() {
+function zmenHodnotu() {
     hodnotaButton.value--;
     if (hodnotaButton.value === 0) {
         pridaniMaxu.value = "maximalizace";
         setTimeout(() => {
             pridaniMaxu.value = "";
-            hodnotaButton.value = 5; 
-        }, 3000); 
-    }};
+            hodnotaButton.value = 5;
+        }, 3000);
+    }
+};
 
 
 </script>
@@ -37,36 +38,47 @@ const pridaniMaxu = ref('');
 
 <style scoped>
 img {
-    width: 5rem;
+    width: 100%;
+    max-width: 5rem;
+    height: auto;
 }
 
 .maximalizace {
-    width: 500px;
+    width: 100%;
+    max-width: 30rem;
+    height: auto;
 }
+
+
 .but {
-    background-color: #7db47e; 
-    color: white; 
-    padding: 15px 32px; /* Vnitřní okraje (top/bottom 15px, left/right 32px) */
-    text-align: center; 
-    text-decoration: none; 
-    display: inline-block; /* Inline blok, aby se tlačítko chovalo jako blokový prvek */
-    font-size: 16px; 
-    margin: 4px 2px; 
-    cursor: pointer; 
-    border: none; 
-    border-radius: 12px; 
-    box-shadow: 0 4px 8px rgba(251, 255, 0, 0.2); /* Stín pro efekt 3D */
-    transition: background-color 0.3s, transform 0.3s; /* Přechod pro změnu barvy a transformaci */
+    background-color: #7db47e;
+    color: white;
+    padding: 15px 32px;
+    /* Vnitřní okraje (top/bottom 15px, left/right 32px) */
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    /* Inline blok, aby se tlačítko chovalo jako blokový prvek */
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border: none;
+    border-radius: 12px;
+    box-shadow: 0 4px 8px rgba(251, 255, 0, 0.2);
+    /* Stín pro efekt 3D */
+    transition: background-color 0.3s, transform 0.3s;
+    /* Přechod pro změnu barvy a transformaci */
 }
 
 .but:hover {
-    background-color: #00ff0d; /* Změna barvy pozadí při najetí myší */
-    transform: scale(1.05); /* A zvětšení tlačítka při najetí myší */
+    background-color: #00ff0d;
+    /* Změna barvy pozadí při najetí myší */
+    transform: scale(1.05);
+    /* A zvětšení tlačítka při najetí myší */
 }
 
 .but:active {
-    transform: scale(0.95); /* Zmenšení tlačítka při kliknutí */
+    transform: scale(0.95);
+    /* Zmenšení tlačítka při kliknutí */
 }
-
-
 </style>
